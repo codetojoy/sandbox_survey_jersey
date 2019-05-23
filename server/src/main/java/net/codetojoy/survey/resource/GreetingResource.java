eb8a92e697d250e130a8774d8c21682c51fbf4e3
Michael Easter <codetojoy@gmail.com>
2019-05-23 19:17:18 -0300

    new Greeting service

--- server/src/main/java/net/codetojoy/survey/resource/GreetingResource.java
+++ server/src/main/java/net/codetojoy/survey/resource/GreetingResource.java
@@ -1,35 +1,21 @@
-package net.codetojoy.survey;
+package net.codetojoy.survey.resource;
 
-import net.codetojoy.survey.model.*;
+import net.codetojoy.survey.model.Greeting;
+import net.codetojoy.survey.service.GreetingService;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
 import java.util.*;
-import java.util.stream.*;
 
 @Path("/greetings")
 public class GreetingResource {
-    // This is obviously a toy example.
-    private static Map<Long,Greeting> mockStorage = new HashMap<>();
-    private static long nextId = 0L;
-
-    static {
-        mockStorage.put(111L, new Greeting(111L, "This is greeting 1"));
-        mockStorage.put(222L, new Greeting(222L, "This is greeting 2"));
-        mockStorage.put(333L, new Greeting(333L, "This is greeting 3"));
-        nextId = 334;
-    }
+    private GreetingService greetingService = new GreetingService();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Greeting> getGreetings() {
-        System.out.println("TRACER 20-MAY SR greetings");
-        
-        List<Greeting> greetings = mockStorage.entrySet()
-                                              .stream()
-                                              .map(Map.Entry::getValue)
-                                              .collect(Collectors.toList());
+        List<Greeting> greetings = greetingService.getGreetings();
 
         return greetings;
     }
@@ -38,25 +24,22 @@ public class GreetingResource {
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Greeting getGreeting(@PathParam("id") long id) {
-        System.out.println("TRACER 20-MAY SR id: " + id);
-        Greeting greeting = mockStorage.get(id);
- 
+        Greeting greeting = greetingService.getGreeting(id);
+
         return greeting;
     }
 
-    // TODO: return location header ??? 
+    // TODO: return location header ???
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public void createGreeting(Greeting greeting) {
-        greeting.setId(nextId);
-        mockStorage.put(nextId, greeting);
-        nextId++; 
-    } 
+        greetingService.createGreeting(greeting);
+    }
 
     @PUT
     @Path("{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     public void updateGreeting(@PathParam("id") long id, Greeting greeting) {
-        mockStorage.put(id, greeting);
-    } 
+        greetingService.updateGreeting(id, greeting);
+    }
 }
