71a94aa5c83790e5f64fa43370ec743ced74a28c
Michael Easter <codetojoy@gmail.com>
2019-05-24 21:09:49 -0300

    incremental change as part of a larger frenzy of activity

--- model/src/main/java/net/codetojoy/survey/model/Answer.java
+++ model/src/main/java/net/codetojoy/survey/model/Answer.java
@@ -7,15 +7,25 @@ public class Answer implements Serializable {
 
     private long id;
     private String desc;
+    private boolean selected;
+    private int score;
 
     public Answer() {
        this.id = Constants.UNKNOWN_ID;
        this.desc = "";
+       this.selected = false;
+       this.score = 0;
+    }
+
+    public Answer(long id, String desc, boolean selected, int score) {
+        this.id = id;
+        this.desc = desc;
+        this.selected = selected;
+        this.score = score;
     }
 
     public Answer(long id, String desc) {
-        this.id = id;
-        this.desc = desc;
+        this(id, desc, false, 0);
     }
 
     @Override
@@ -24,6 +34,8 @@ public class Answer implements Serializable {
 
         buffer.append("id: " + id);
         buffer.append(" desc: " + desc);
+        buffer.append(" selected: " + selected);
+        buffer.append(" score: " + score);
 
         return buffer.toString();
     }
@@ -44,5 +56,21 @@ public class Answer implements Serializable {
 
     public void setDesc(String desc) {
         this.desc = desc;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 }
